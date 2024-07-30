46c22278877973b9794fd38096670365f935cc1e
rherrell <russ.herrell@hpe.com>
2024-07-30 15:33:22 -0600

    added Signed code and copyright
    Signed-off-by: rherrell <russ.herrell@hpe.com>

--- app.py
+++ app.py
@@ -1,90 +1,99 @@
 # Copyright IBM Corp. 2023
+# Copyright Hewlett Packard Enterprise Development LP 2024
 # This software is available to you under a BSD 3-Clause License. 
 # The full license terms are available here: https://github.com/OpenFabrics/sunfish_server_reference/blob/main/LICENSE
+import os
+import traceback
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 from sunfish.lib.core import Core
 from sunfish.lib.exceptions import *
 import logging
 
-logger = logging.getLogger(__name__)
+FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
-# normally would read config in from a file, here it is hardcoded
+logger = logging.getLogger("Server")
 conf = {
-    "storage_backend": "FS",
+    "storage_backend" : {
+        "module_name": "storage.my_storage_package.my_storage_backend",
+        "class_name": "StorageBackend"
+        },
 	"redfish_root": "/redfish/v1/",
-	"handlers" : {
-		"subscription_handler":"redfish",
-		"event_handler" :"redfish"
-	},
 	"backend_conf" : {
 		"fs_root": "Resources",
-		"subscribers_root": "EventService/Subscriptions"
+		"subscribers_root": "EventService/Subscriptions",
+		"clean_resource_path": "../../Sunfish/server_start_Resources" 
+	},
+	"handlers": {
+		"subscription_handler": "redfish",
+		"event_handler": "redfish"
 	}
 }
 
 # initialize flask
 
-app = Flask(__name__)
+template_dir = os.path.abspath('./templates_web')
+app = Flask(__name__, template_folder=template_dir)
 sunfish_core = Core(conf)
 
-# Usa codici http
-@app.route('/<path:resource>', methods=["GET"])
-def get(resource):
+@app.route('/browse')
+def browse():
+    return render_template('browse.html')
 
+# Usa codici http
+@app.route('/<path:resource>', methods=["GET"], strict_slashes=False)
+def get(resource):
+	logger.debug(f"GET on: {request.path}")
 	try:
-		# need to fix up path to redfish resource
-		# Flask strips off ALL leading '/'s, but Sunfish requires it
-		resource = "/"+resource
-		logger.debug(f"resource: {resource}")
-		resp = sunfish_core.get_object(resource)
+		resp = sunfish_core.get_object(request.path)
 		return resp, 200
 	except ResourceNotFound as e:
 		return e.message, 404
 
-@app.route('/<path:resource>', methods=["POST"])
+@app.route('/<path:resource>', methods=["POST"], strict_slashes=False)
 def post(resource):
+	logger.debug("POST")
 	try :
-		resource = "/"+resource
-		logger.debug(f"resource: {resource}")
-		data = request.json
-		logger.debug(f"data: {data}")
-		resp = sunfish_core.create_object(resource, request.json)
+		if resource == "EventListener":
+			resp = sunfish_core.handle_event(request.json)
+		elif resource == "ResetResources":
+			resp = sunfish_core.storage_backend.reset_resources(conf['backend_conf']['fs_root'],conf['backend_conf']['clean_resource_path'])
+		else:
+			resp = sunfish_core.create_object(request.path, request.json)
 		return resp
 	except CollectionNotSupported as e:
 		return e.message, 405 # method not allowed
 	except AlreadyExists as e:
 		return e.message, 409 # Conflict
+	except PropertyNotFound as e:
+		return e.message, 400
 
-@app.route('/<path:resource>', methods=["PUT"])
+@app.route('/<path:resource>', methods=["PUT"], strict_slashes=False)
 def put(resource):
 	try:
-		resource = "/"+resource
-		logger.debug(f"resource: {resource}")
 		data = request.json
-		logger.debug(f"data: {data}")
-		resp = sunfish_core.replace_object(resource, data)
+		resp = sunfish_core.replace_object(data)
 		return resp, 200
 	except ResourceNotFound as e:
 		return e.message, 404
 
-@app.route('/<path:resource>', methods=["PATCH"])
+@app.route('/<path:resource>', methods=["PATCH"], strict_slashes=False)
 def patch(resource):
 	try:
-		resource = "/"+resource
-		logger.debug(f"resource: {resource}")
+		logger.debug("PATCH")
 		data = request.json
-		resp = sunfish_core.patch_object(resource, data)
+		resp = sunfish_core.patch_object(path=request.path, payload=data)
 		return resp, 200
 	except ResourceNotFound as e:
 		return e.message, 404
+	except Exception:
+		traceback.print_exc()
 
-@app.route('/<path:resource>', methods=["DELETE"])
+@app.route('/<path:resource>', methods=["DELETE"], strict_slashes=False)
 def delete(resource):
 	try:
-		resource = "/"+resource
-		logger.debug(f"resource: {resource}")
-		resp = sunfish_core.delete_object(resource)
+		resp = sunfish_core.delete_object(request.path)
 		return resp, 200
 	except ResourceNotFound as e:
 		return e.message, 404
@@ -94,4 +103,4 @@ def delete(resource):
 		return e.message, 400
 
 # we run app debugging mode
-#app.run(debug=False)
+app.run(debug=False)
